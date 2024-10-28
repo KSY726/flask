5c76b19fc74346f5897daf827aee05bb452d51d8
KSY726 <syku0903@gmail.com>
2024-10-28 12:03:09 +0900

    1

--- test2.py
+++ test2.py
@@ -9,11 +9,15 @@ def hello_world():
 @app.route('/method', methods=['GET', 'POST'])
 def method():
     if request.method == 'GET':
-        return "GET으로 전달"
+        # args_dict = request.args.to_dict()
+        # print(args_dict)
+        num = request.args["num"]
+        name = request.args.get("name")
+        return "GET으로 전달된 데이터({}, {})".format(num, name)
     else:
-        return "POST로 전달"
+        num = request.form["num"]
+        name = request.form["name"]
+        return "POST로 전달된 데이터({}, {})".format(num, name)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
