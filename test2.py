b1b74b5ec765a7801bc588fea5904fd18d150e3f
KSY726 <syku0903@gmail.com>
2024-10-28 11:52:26 +0900

    10/28

---
+++ test2.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'abcdabcd'
+
+@app.route('/method', methods=['GET', 'POST'])
+def method():
+    if request.method == 'GET':
+        return "GET으로 전달"
+    else:
+        return "POST로 전달"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
