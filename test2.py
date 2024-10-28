ae3886e283061654474c6004dd9f218f872c7e86
KSY726 <syku0903@gmail.com>
2024-10-28 12:14:03 +0900

    10/28

--- test2.py
+++ test2.py
@@ -1,23 +1,26 @@
-from flask import Flask, request
-
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/weather')
+def weather():
+    return 'Hello, weather!'
+
 @app.route('/naver')
-def hello_world():
-    return 'abcdabcd'
+def naver():
+    return render_template('naver.html')
 
 @app.route('/method', methods=['GET', 'POST'])
 def method():
     if request.method == 'GET':
-        # args_dict = request.args.to_dict()
-        # print(args_dict)
-        num = request.args["num"]
-        name = request.args.get("name")
-        return "GET으로 전달된 데이터({}, {})".format(num, name)
+        return "GET으로 전달"
     else:
-        num = request.form["num"]
-        name = request.form["name"]
-        return "POST로 전달된 데이터({}, {})".format(num, name)
+        keyword = request.form["keyword"]
+        print(keyword)
+        return f"POST로 전달된 당신이 입력한 검색어: {keyword}"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
